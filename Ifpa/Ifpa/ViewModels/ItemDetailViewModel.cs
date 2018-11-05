4451852ee41e1932ee043a70c085942d0ba8e90f
Ed Giardina <ed@litmus.com>
2018-11-05 09:58:41 -0500

    Add project files.

---
+++ Ifpa/Ifpa/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Ifpa.Models;
+
+namespace Ifpa.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
