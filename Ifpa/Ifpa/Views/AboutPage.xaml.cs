73dca5cccdd7e5951621d043c82bddc6c4c95bba
Ed Giardina <ed@litmus.com>
2018-11-09 21:25:37 -0500

    Link to my IFPA profile in the About page

--- Ifpa/Ifpa/Views/AboutPage.xaml.cs
+++ Ifpa/Ifpa/Views/AboutPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
-
+﻿using Ifpa.ViewModels;
+using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,9 +9,16 @@ namespace Ifpa.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutPage : ContentPage
     {
+        private int creatorIfpaNumber = 16927;
+
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private async Task CreatorLabel_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PlayerDetailPage(new PlayerDetailViewModel(creatorIfpaNumber)));
         }
     }
 }
