19b6c5a506c933fb9c18e37e44693f1c45a86090
Ed Giardina <ed@litmus.com>
2018-11-18 23:02:13 -0500

    Add setting calendar item to your local iOS/Android calendar, closes #21

---
+++ Ifpa/Ifpa.Android/AndroidReminderService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Provider;
+using Ifpa.Droid;
+using Ifpa.Interfaces;
+using Ifpa.ViewModels;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndroidReminderService))]
+namespace Ifpa.Droid
+{
+    public class AndroidReminderService : IReminderService
+    {
+        public async Task<bool> CreateReminder(CalendarDetailViewModel calendarDetail)
+        {
+            Intent intent = new Intent(Intent.ActionInsert);
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Title, calendarDetail.TournamentName);
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Description, calendarDetail.Details);            
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Dtstart, new DateTimeOffset(calendarDetail.StartDate).ToUnixTimeMilliseconds());
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Dtend, new DateTimeOffset(calendarDetail.EndDate).ToUnixTimeMilliseconds());
+
+            intent.PutExtra(CalendarContract.EventsColumns.EventLocation, $"{calendarDetail.Address1} {calendarDetail.City} {calendarDetail.State}");
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.EventTimezone, "UTC");
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.EventEndTimezone, "UTC");
+            intent.SetData(CalendarContract.Events.ContentUri);
+            Forms.Context.StartActivity(intent);
+            return true;
+        }
+    }
+}
---
+++ Ifpa/Ifpa.iOS/iOSReminderService.cs
@@ -0,0 +1,58 @@
+﻿using EventKit;
+using Foundation;
+using Ifpa.Interfaces;
+using Ifpa.iOS;
+using Ifpa.ViewModels;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(iOSReminderService))]
+namespace Ifpa.iOS
+{
+    public class iOSReminderService : IReminderService
+    {
+        protected EKEventStore eventStore = new EKEventStore();
+
+        public async Task<bool> CreateReminder(CalendarDetailViewModel calendarDetail)
+        {
+            var granted = await eventStore.RequestAccessAsync(EKEntityType.Event);//, (bool granted, NSError e) =>
+            if (granted.Item1)
+            {
+                EKEvent newEvent = EKEvent.FromStore(eventStore);
+                newEvent.StartDate = DateTimeToNSDate(calendarDetail.StartDate);
+                newEvent.EndDate = DateTimeToNSDate(calendarDetail.EndDate);
+                newEvent.Title = calendarDetail.TournamentName;
+                newEvent.Notes = calendarDetail.Details;
+                newEvent.Calendar = eventStore.DefaultCalendarForNewEvents;
+                //TODO: we don't get start/end time for these so fix so its not all day
+                newEvent.AllDay = true;
+                newEvent.Location = $"{calendarDetail.Address1} {calendarDetail.City} {calendarDetail.State}";
+                NSError e;
+                eventStore.SaveEvent(newEvent, EKSpan.ThisEvent, out e);
+                return true;
+            }
+            else
+            {               
+                return false;
+            }
+        }
+
+        private DateTime NSDateToDateTime(NSDate date)
+        {
+            double secs = date.SecondsSinceReferenceDate;
+            if (secs < -63113904000)
+                return DateTime.MinValue;
+            if (secs > 252423993599)
+                return DateTime.MaxValue;
+            return (DateTime)date;
+        }
+
+        private NSDate DateTimeToNSDate(DateTime date)
+        {
+            if (date.Kind == DateTimeKind.Unspecified)
+                date = DateTime.SpecifyKind(date, DateTimeKind.Local);
+            return (NSDate)date;
+        }
+    }
+}
