193ffc5119066d8ef6d6fa59fb59465c66be35c8
Ed Giardina <ed@litmus.com>
2018-12-11 20:56:49 -0500

    First pass at News Reader in-app

---
+++ Ifpa/Ifpa/Views/NewsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Ifpa.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ifpa.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewsPage : ContentPage
+    {
+        NewsViewModel viewModel;
+
+        public NewsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = new NewsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.NewsItems.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        private void ItemsListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
