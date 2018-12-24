bde04b8a45be0e9ab5626893d541f605ab2497b6
Ed Giardina <ed@litmus.com>
2018-12-23 21:16:35 -0500

    Moving Activity feed list display into its own templates

--- Ifpa/Ifpa.Android/Resources/Resource.designer.cs
+++ Ifpa/Ifpa.Android/Resources/Resource.designer.cs
@@ -2521,26 +2521,26 @@ namespace Ifpa.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020155
-			public const int avd_hide_password_1 = 2130837845;
-			
 			// aapt resource value: 0x7f020156
-			public const int avd_hide_password_2 = 2130837846;
+			public const int avd_hide_password_1 = 2130837846;
 			
 			// aapt resource value: 0x7f020157
-			public const int avd_hide_password_3 = 2130837847;
+			public const int avd_hide_password_2 = 2130837847;
+			
+			// aapt resource value: 0x7f020158
+			public const int avd_hide_password_3 = 2130837848;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020158
-			public const int avd_show_password_1 = 2130837848;
-			
 			// aapt resource value: 0x7f020159
-			public const int avd_show_password_2 = 2130837849;
+			public const int avd_show_password_1 = 2130837849;
 			
 			// aapt resource value: 0x7f02015a
-			public const int avd_show_password_3 = 2130837850;
+			public const int avd_show_password_2 = 2130837850;
+			
+			// aapt resource value: 0x7f02015b
+			public const int avd_show_password_3 = 2130837851;
 			
 			// aapt resource value: 0x7f020055
 			public const int calendar = 2130837589;
@@ -3274,11 +3274,11 @@ namespace Ifpa.Droid
 			// aapt resource value: 0x7f020148
 			public const int notification_icon_background = 2130837832;
 			
-			// aapt resource value: 0x7f020153
-			public const int notification_template_icon_bg = 2130837843;
-			
 			// aapt resource value: 0x7f020154
-			public const int notification_template_icon_low_bg = 2130837844;
+			public const int notification_template_icon_bg = 2130837844;
+			
+			// aapt resource value: 0x7f020155
+			public const int notification_template_icon_low_bg = 2130837845;
 			
 			// aapt resource value: 0x7f020149
 			public const int notification_tile_bg = 2130837833;
@@ -3293,22 +3293,25 @@ namespace Ifpa.Droid
 			public const int refresh = 2130837836;
 			
 			// aapt resource value: 0x7f02014d
-			public const int search = 2130837837;
+			public const int ribbon = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int splash_background = 2130837838;
+			public const int search = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int star = 2130837839;
+			public const int splash_background = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int tab_about = 2130837840;
+			public const int star = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int tooltip_frame_dark = 2130837841;
+			public const int tab_about = 2130837841;
 			
 			// aapt resource value: 0x7f020152
-			public const int tooltip_frame_light = 2130837842;
+			public const int tooltip_frame_dark = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int tooltip_frame_light = 2130837843;
 			
 			static Drawable()
 			{
---
+++ Ifpa/Ifpa/Views/DataTemplates/ActivityFeedTournamentResultViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ifpa.Views.DataTemplates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ActivityFeedTournamentResultViewCell : ViewCell
+	{
+		public ActivityFeedTournamentResultViewCell()
+		{
+			InitializeComponent();
+		}
+	}
+}
