de8a1f39cd4d6a9285bb360c5280f686b5a1507d
Ed Giardina <ed@litmus.com>
2019-01-14 23:49:52 -0500

    Adding settings page and settings singleton

---
+++ Ifpa/Ifpa/Views/SettingsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Ifpa.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ifpa.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new SettingsViewModel();
+		}
+	}
+}
