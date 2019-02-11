1c45ad872a8a542fd9451945af6485c16f90bb97
Ed Giardina <ed@litmus.com>
2019-02-10 22:31:47 -0500

    Scaffolding stats page

---
+++ Ifpa/Ifpa/Views/StatsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Ifpa.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ifpa.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatsPage : ContentPage
+    {
+        private StatsViewModel viewModel;
+
+        public StatsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = new StatsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
