a0f7760a89fef337e1a71aced6e8e18853985253
Ed Giardina <ed@litmus.com>
2019-03-10 14:49:46 -0400

    first draft of a today-widget

---
+++ Ifpa/Ifpa.iOS.RankExtension/CodeBasedViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using NotificationCenter;
+using UIKit;
+
+namespace Ifpa.iOS.RankExtension
+{
+    [Register("CodeBasedViewController")]
+    public class CodeBasedViewController : UIViewController, INCWidgetProviding
+    {
+        public CodeBasedViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Add label to view
+            var TodayMessage = new UILabel(new CGRect(0, 0, View.Frame.Width, View.Frame.Height))
+            {
+                TextAlignment = UITextAlignment.Center
+            };
+
+            View.AddSubview(TodayMessage);
+
+            TodayMessage.Text = "Your rank is 206th";
+
+        }
+    }
+}
--- Ifpa/Ifpa/Models/Constants.cs
+++ Ifpa/Ifpa/Models/Constants.cs
@@ -6,8 +6,8 @@ namespace Ifpa.Models
 {
     public static class Constants
     {
-        public static string IfpaApiKey => "";
-        public static string SyncFusionLicenseKey => "";
+        public static string IfpaApiKey => "585c0438147e11520622277d2ac7b298";
+        public static string SyncFusionLicenseKey => "NTE5ODRAMzEzNjJlMzQyZTMwSkhKWXhrN3o0aXZKYjAvNzdKL2w0TlZjUDJZTmJlYjVCdkJPTnJsdHQyZz0=";
         public static string AppStoreAppId => "1441736303";
         public static string PlayStoreAppId => "com.edgiardina.ifpa";
         public static double MetersInAMile => 1609.344;
