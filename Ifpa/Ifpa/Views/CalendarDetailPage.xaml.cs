e358026666a3f93574357666b20de71022f0657f
Ed Giardina <ed@litmus.com>
2019-04-27 16:50:42 -0400

    Fixes #68

--- Ifpa/Ifpa/Views/CalendarDetailPage.xaml.cs
+++ Ifpa/Ifpa/Views/CalendarDetailPage.xaml.cs
@@ -30,16 +30,22 @@ namespace Ifpa.Views
             //when busy is flipped back to false it means we are done loading the address
             if (e.PropertyName == "IsBusy" && viewModel.IsBusy == false)
             {
-                var locations =  await Geocoding.GetLocationsAsync(viewModel.Address1 + " " + viewModel.City + ", " + viewModel.State);                                                        
-
-                var position = new Position(locations.First().Latitude, locations.First().Longitude);
-                calendarMap.MoveToRegion(new MapSpan(position, 0.1, 0.1));
-                calendarMap.Pins.Add(new Pin
+                try
                 {
-                    Label = viewModel.TournamentName,
-                    Position = position
-                });
-                calendarMap.IsVisible = true;
+                    var locations = await Geocoding.GetLocationsAsync(viewModel.Address1 + " " + viewModel.City + ", " + viewModel.State);
+
+                    var position = new Position(locations.First().Latitude, locations.First().Longitude);
+                    calendarMap.MoveToRegion(new MapSpan(position, 0.1, 0.1));
+                    calendarMap.Pins.Add(new Pin
+                    {
+                        Label = viewModel.TournamentName,
+                        Position = position
+                    });
+
+                    calendarMap.IsVisible = true;
+                }
+                //unable to geocode position on the map, ignore. 
+                catch { }                
             }
         }
 
