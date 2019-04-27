8e5dc3aa4f96859a204da1fee667720a9557f146
Ed Giardina <ed@litmus.com>
2019-04-26 21:04:36 -0400

    Adding initial NACS data

---
+++ Ifpa/Ifpa/Views/PlayerChampionshipSeriesPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Ifpa.ViewModels;
+using PinballApi.Models.WPPR.v1.Players;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ifpa.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerChampionshipSeriesPage : ContentPage
+    {
+        PlayerChampionshipSeriesViewModel viewModel;
+        public ObservableCollection<string> Items { get; set; }
+
+        public PlayerChampionshipSeriesPage(PlayerChampionshipSeriesViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //var tournament = e.Item as Result;
+            //if (tournament == null)
+            //    return;
+
+            //await Navigation.PushAsync(new TournamentResultsPage(new TournamentResultsViewModel(tournament.TournamentId)));
+
+            ////Deselect Item
+            //((ListView)sender).SelectedItem = null;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.ChampionshipSeries.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }  
+    }
+}
