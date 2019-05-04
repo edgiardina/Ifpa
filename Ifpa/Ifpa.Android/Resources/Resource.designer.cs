2ffff0c9a48f7a69a05bd4b7b0ea13a86f3592c6
Ed Giardina <ed@litmus.com>
2019-05-03 23:03:35 -0400

    Added Championship Series summary page under 'More'

--- Ifpa/Ifpa.Android/Resources/Resource.designer.cs
+++ Ifpa/Ifpa.Android/Resources/Resource.designer.cs
@@ -5960,26 +5960,26 @@ namespace Ifpa.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
-			// aapt resource value: 0x7f020166
-			public const int avd_hide_password_1 = 2130837862;
-			
 			// aapt resource value: 0x7f020167
-			public const int avd_hide_password_2 = 2130837863;
+			public const int avd_hide_password_1 = 2130837863;
 			
 			// aapt resource value: 0x7f020168
-			public const int avd_hide_password_3 = 2130837864;
+			public const int avd_hide_password_2 = 2130837864;
+			
+			// aapt resource value: 0x7f020169
+			public const int avd_hide_password_3 = 2130837865;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
-			// aapt resource value: 0x7f020169
-			public const int avd_show_password_1 = 2130837865;
-			
 			// aapt resource value: 0x7f02016a
-			public const int avd_show_password_2 = 2130837866;
+			public const int avd_show_password_1 = 2130837866;
 			
 			// aapt resource value: 0x7f02016b
-			public const int avd_show_password_3 = 2130837867;
+			public const int avd_show_password_2 = 2130837867;
+			
+			// aapt resource value: 0x7f02016c
+			public const int avd_show_password_3 = 2130837868;
 			
 			// aapt resource value: 0x7f020057
 			public const int calendar = 2130837591;
@@ -6740,11 +6740,11 @@ namespace Ifpa.Droid
 			// aapt resource value: 0x7f020153
 			public const int notification_icon_background = 2130837843;
 			
-			// aapt resource value: 0x7f020164
-			public const int notification_template_icon_bg = 2130837860;
-			
 			// aapt resource value: 0x7f020165
-			public const int notification_template_icon_low_bg = 2130837861;
+			public const int notification_template_icon_bg = 2130837861;
+			
+			// aapt resource value: 0x7f020166
+			public const int notification_template_icon_low_bg = 2130837862;
 			
 			// aapt resource value: 0x7f020154
 			public const int notification_tile_bg = 2130837844;
@@ -6793,6 +6793,9 @@ namespace Ifpa.Droid
 			
 			// aapt resource value: 0x7f020163
 			public const int tooltip_frame_light = 2130837859;
+			
+			// aapt resource value: 0x7f020164
+			public const int trophy = 2130837860;
 			
 			static Drawable()
 			{
---
+++ Ifpa/Ifpa/Views/ChampionshipSeriesPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Ifpa.ViewModels;
+using PinballApi.Models.WPPR.v2.Nacs;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ifpa.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChampionshipSeriesPage : ContentPage
+    {
+        ChampionshipSeriesViewModel viewModel;
+
+        public ChampionshipSeriesPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = new ChampionshipSeriesViewModel();
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var championshipStandings = e.Item as NacsStandings;
+            if (championshipStandings == null)
+                return;
+
+            await Navigation.PushAsync(new ChampionshipSeriesDetailPage(new ChampionshipSeriesDetailViewModel(championshipStandings.StateProvince)));
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.StateProvinceStandings.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }  
+    }
+}
