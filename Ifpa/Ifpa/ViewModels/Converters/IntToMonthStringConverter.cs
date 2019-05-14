efb4db2de6c1657038346a6eac3fb4b8154a4382
Ed Giardina <ed@litmus.com>
2019-05-13 23:09:56 -0400

    merge back

---
+++ Ifpa/Ifpa/ViewModels/Converters/IntToMonthStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Ifpa.ViewModels.Converters
+{
+    public class IntToMonthStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new DateTime(DateTime.Now.Year, (int)value, 1).ToString("MMMM", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new MissingMethodException("Method Not implemented");
+        }
+    }
+}
