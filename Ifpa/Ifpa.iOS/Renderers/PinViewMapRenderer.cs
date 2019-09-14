26a60c3c6251ca40dfb908e0498882eded7d6d4d
Ed Giardina <ed@litmus.com>
2019-09-14 10:38:27 -0400

    Auto-open maps in iOS

---
+++ Ifpa/Ifpa.iOS/Renderers/PinViewMapRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Ifpa.iOS.Renderers;
+using Ifpa.Views.Controls;
+using MapKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(PinViewMap), typeof(PinViewMapRenderer))]
+namespace Ifpa.iOS.Renderers
+{
+    public class PinViewMapRenderer : MapRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                var map = Control as MKMapView;
+                map.DidDeselectAnnotationView += (object sender, MKAnnotationViewEventArgs eventArgs) =>
+                {
+                    foreach (var anno in ((MKMapView)sender).Annotations)
+                    {                       
+                        ((MKMapView)sender).SelectAnnotation(anno, true);
+                    }
+                };
+                map.DidFinishRenderingMap += (object sender, MKDidFinishRenderingMapEventArgs eventArgs) =>
+                {
+                    foreach (var anno in ((MKMapView)sender).Annotations)
+                    {
+                        ((MKMapView)sender).SelectAnnotation(anno, true);
+                    }
+                };
+
+            }
+        }
+    }
+}
---
+++ Ifpa/Ifpa/Views/Controls/PinViewMap.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms.Maps;
+
+namespace Ifpa.Views.Controls
+{
+    /// <summary>
+    /// This class is an override which, using custom renderers, will automatically open / expand any pins on the map
+    /// </summary>
+    public class PinViewMap : Map
+    {
+    }
+}
