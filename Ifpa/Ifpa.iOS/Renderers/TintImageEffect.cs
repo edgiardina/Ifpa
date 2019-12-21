3c271ff81a1deacaa290c6cc177cc5929decd2f8
Ed Giardina <ed@litmus.com>
2019-12-20 21:43:03 -0500

    Adding tint effect

---
+++ Ifpa/Ifpa.iOS/Renderers/TintImageEffect.cs
@@ -0,0 +1,35 @@
+﻿using Ifpa.iOS.Renderers;
+using System;
+using System.Linq;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using FormsTintImageEffect = Ifpa.Effects.TintImageEffect;
+
+[assembly: ResolutionGroupName(FormsTintImageEffect.GroupName)]
+[assembly: ExportEffect(typeof(TintImageEffect), FormsTintImageEffect.Name)]
+namespace Ifpa.iOS.Renderers
+{
+    public class TintImageEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var effect = (FormsTintImageEffect)Element.Effects.FirstOrDefault(e => e is FormsTintImageEffect);
+
+                if (effect == null || !(Control is UIImageView image))
+                    return;
+
+                image.Image = image.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+                image.TintColor = effect.TintColor.ToUIColor();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"An error occurred when setting the {typeof(TintImageEffect)} effect: {ex.Message}\n{ex.StackTrace}");
+            }
+        }
+
+        protected override void OnDetached() { }
+    }
+}
