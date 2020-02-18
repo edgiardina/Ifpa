0b566d56dac49731705a2827c46d21867f35e9a8
Ed Giardina <ed@litmus.com>
2020-02-17 22:09:57 -0500

    performance improvements for the calendar page

---
+++ Ifpa/Ifpa/Models/CalendarEventCollectionRange.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Ifpa.Models
+{
+    public class CalendarEventCollectionRange : CalendarEventCollection
+    {
+        private bool _suppressNotification = false;
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (!_suppressNotification)
+                base.OnCollectionChanged(e);
+        }
+
+        public void AddRange(IEnumerable<CalendarInlineEvent> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            _suppressNotification = true;
+
+            foreach (CalendarInlineEvent item in list)
+            {
+                Add(item);
+            }
+            _suppressNotification = false;
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }        
+    }
+}
